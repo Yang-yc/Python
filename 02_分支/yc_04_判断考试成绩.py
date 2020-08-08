c82819432b1642ba55f38821914c3c7d29835330
Yang-yc <ycc20121404@163.com>
2020-08-08 09:32:06 +0800

    提交

---
+++ 02_分支/yc_04_判断考试成绩.py
@@ -0,0 +1,9 @@
+# 定义两个整数变量python_score、c_score，编写代码判断成绩
+python_score = 80
+c_score = 75
+
+# 要求只要有一门成绩大于60分就算合格
+if python_score > 60 or c_score > 60:
+    print("考试通过")
+else:
+    print("考试失败，继续努力")
