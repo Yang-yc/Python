d6084e1f874d2380b81d143a2ebd620b400f4151
Yang-yc <ycc20121404@163.com>
2020-08-29 21:30:44 +0800

    提交

---
+++ 11_模块/yc_05_from_impot导入.py
@@ -0,0 +1,7 @@
+from yc_01_测试模块1 import Dog
+from yc_02_测试模块2 import say_hello
+
+say_hello()
+
+wangcai = Dog()
+print(wangcai)
---
+++ 11_模块/yc_06_from_impot导入注意事项.py
@@ -0,0 +1,4 @@
+from yc_01_测试模块1 import say_hello
+from yc_02_测试模块2 import  say_hello
+
+say_hello()
